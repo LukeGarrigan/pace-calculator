2418753ddc60711afbe7650fd31a5fc7711fa9e9
Luke <Lukegarrigan8@gmail.com>
2021-08-14 16:14:14 +0100

    Time/Pace/Settings done

--- app/src/main/java/com/example/pace_calc/MainActivity.kt
+++ app/src/main/java/com/example/pace_calc/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.example.pace_calc
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.MenuItem
-import android.view.inputmethod.EditorInfo
-import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.example.pace_calc.fragments.HelpFragment
+import com.example.pace_calc.fragments.SettingsFragment
 import com.example.pace_calc.fragments.PaceFragment
 import com.example.pace_calc.fragments.TimeFragment
-import com.google.android.material.bottomnavigation.BottomNavigationItemView
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         val timeFragment = TimeFragment();
         val paceFragment = PaceFragment();
-        val helpFragment = HelpFragment();
+        val settingsFragment = SettingsFragment();
         makeCurrentFragment(timeFragment);
 
 
@@ -32,7 +25,7 @@ class MainActivity : AppCompatActivity() {
                 when (it.itemId) {
                     R.id.timeId -> makeCurrentFragment(timeFragment)
                     R.id.paceId -> makeCurrentFragment(paceFragment)
-                    R.id.helpId -> makeCurrentFragment(helpFragment)
+                    R.id.helpId -> makeCurrentFragment(settingsFragment)
                 }
                 true;
         }
